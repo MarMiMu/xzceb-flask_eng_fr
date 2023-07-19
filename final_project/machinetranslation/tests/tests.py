ca70aff47bff425ed5531e55dc641fb4163e405e
Mikha <mikha.muliadi@gmail.com>
2023-07-19 17:21:23 -0400

    changed unit tests

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -5,8 +5,10 @@ from translator import french_to_english
 class TestMethods(unittest.TestCase):
     def test_e2f(self):
         self.assertEqual(english_to_french("Hello"),"Bonjour")
+        self.assertNotEqual(english_to_french("Bye"),"Bonjour")
     def test_f2e(self):
         self.assertEqual(french_to_english("Bonjour"),"Hello")
+        self.assertNotEqual(english_to_french("Bonjour"),"Bye")
 
 if __name__ == '__main__':
     unittest.main()
